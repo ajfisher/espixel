1e64e21d5b030e884bfc1b6ba0b7617cb8b690ab
ajfisher <ajfisher.td@gmail.com>
2016-12-05 23:24:25 +1100

    add method for testing

---
+++ utils/colour_test.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+declare -a colours=("880000" "008800" "000088" "888800" "008888" "880088" "888888" "000000")
+
+mqtt=mosquitto_pub
+
+if [ -z "$1" ]
+then
+    stripid=0
+else
+    stripid=$1
+fi
+
+if [ -z "$2" ]
+then
+    sleeptime=1
+else
+    sleeptime=$2
+fi
+
+if [ -z "$3" ]
+then
+    nodetarget="ESP_f2df79"
+else
+    nodetarget="$3"
+fi
+
+
+# while true construct
+while :
+do
+    $mqtt -t "${nodetarget}/${stripid}/i/c/strip" -m "000000"
+    echo "Strip animate 10 seconds"
+    $mqtt -t "${nodetarget}/${stripid}/i/c/px/1" -m "888800"
+    $mqtt -t "${nodetarget}/${stripid}/i/c/anim/shift" -m "1:100:10000:1:1"
+    sleep 9
+    echo "should stop imminently"
+    sleep 3
+
+    echo "go backwards 5 seconds"
+    $mqtt -t "${nodetarget}/${stripid}/i/c/anim/shift" -m "1:100:5000:0:1"
+    sleep 4
+    echo "should stop imminently"
+    sleep 3
+
+    echo "Colour cycle"
+    for colour in "${colours[@]}"
+    do
+        $mqtt -t "${nodetarget}/${stripid}/i/c/strip" -m "$colour"
+        sleep ${sleeptime}
+    done
+done
